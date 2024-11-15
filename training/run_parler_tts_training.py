c00befb1d786105529bf5f68cb31f21be402a43a
Afrizal Hasbi Azizy <afrizal.azizy@gmail.com>
2024-11-15 13:28:30 +0700

    Update run_parler_tts_training.py

--- training/run_parler_tts_training.py
+++ training/run_parler_tts_training.py
@@ -83,7 +83,7 @@ def main():
 
     # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
     # information sent is the one passed as arguments along with your Python/PyTorch versions.
-    send_example_telemetry("run_parler_tts", model_args, data_args)
+    # send_example_telemetry("run_parler_tts", model_args, data_args)
 
     if training_args.dtype == "float16":
         mixed_precision = "fp16"
